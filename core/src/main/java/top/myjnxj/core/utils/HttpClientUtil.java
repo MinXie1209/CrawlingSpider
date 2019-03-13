ba70d560a8d8777f68b5c9ed1f52a926ad9f58ea
江南小俊 <admin@example.com>
2019-03-13 10:50:01 +0800

    使用连接池管理连接

--- core/src/main/java/top/myjnxj/core/utils/HttpClientUtil.java
+++ core/src/main/java/top/myjnxj/core/utils/HttpClientUtil.java
@@ -1,37 +1,45 @@
 package top.myjnxj.core.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
+import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
+import top.myjnxj.core.Spider;
 import top.myjnxj.core.model.Request;
+import top.myjnxj.core.model.Task;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 
 @Slf4j
 public class HttpClientUtil {
 
+    private static HttpClient client = null;
 
     public static void main(String[] args) throws IOException {
-        String url = "http://central.maven.org/maven2/";
-        String result = HttpClientUtil.get(new Request("http://www.baidu.com"));
-        log.info("url:{},  result:{}", url, result);
+        while (true){
+            String url = "https://www.toutiao.com//";
+            String result = HttpClientUtil.get(new Request(url), new Spider());
+            log.info("url:{},  result:{}", url, result);
+        }
+
     }
 
-    public static String get(Request request) throws IOException {
+
+    public static String get(Request request, Task task) throws IOException {
         String body = null;
         //创建httpClient实例
-        HttpClient httpClient = HttpClients.createDefault();
+        HttpClient httpClient = getClient();
         HttpGet httpGet = null;
-        List<NameValuePair> params=request.getParams();
-        String url=request.getUrl();
+        List<NameValuePair> params = request.getParams();
+        String url = request.getUrl();
         if (params != null) {
             StringBuilder stringBuilder = new StringBuilder(url);
             stringBuilder.append("?");
@@ -45,7 +53,13 @@ public class HttpClientUtil {
             //get
             httpGet = new HttpGet(url);
         }
-        httpGet.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36");
+        if(task!=null&&task.getSite()!=null){
+            Map<String,String> map=task.getSite().getHeaders();
+            for (String key:map.keySet()){
+                httpGet.addHeader(key,map.get(key));
+            }
+            httpGet.setHeader("User-Agent",task.getSite().getUserAgent());
+        }
         /*HttpHost proxy=new HttpHost("119.122.212.110",9000);
         RequestConfig config= RequestConfig.custom()
                 .setProxy(proxy)
@@ -56,7 +70,7 @@ public class HttpClientUtil {
         CloseableHttpResponse response = null;
         response = (CloseableHttpResponse) httpClient.execute(httpGet);
         HttpEntity httpEntity = response.getEntity();
-       // log.info("content-type:{}", httpEntity.getContentType());
+        // log.info("content-type:{}", httpEntity.getContentType());
 
         //  InputStream in=httpEntity.getContent();
         //  FileUtils.copyToFile(in,new File("D://123.png"));
@@ -64,5 +78,19 @@ public class HttpClientUtil {
         response.close();
         ((CloseableHttpClient) httpClient).close();
         return body;
+    }
+
+    private static HttpClient getClient() {
+        if (client == null) {
+           PoolingHttpClientConnectionManager connectionManager=new PoolingHttpClientConnectionManager();
+           connectionManager.setMaxTotal(200);
+           connectionManager.setDefaultMaxPerRoute(100);
+           CloseableHttpClient httpClient=HttpClients.custom()
+                   .setConnectionManager(connectionManager)
+                   .setConnectionManagerShared(true)
+                   .build();
+           client=httpClient;
+        }
+        return client;
     }
 }
